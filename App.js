bb86f91fc9a7e8c01bf103af794c2b479182b192
Alejandro <alejandrochang10@gmail.com>
2019-02-11 17:06:21 -0800

    initial project setup

---
+++ App.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Something Changed!</Text>
+        <View style={styles.textInput}>
+          <TextInput
+            placeholder="Search for a Coffeeshop"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  textInput: {
+    textAlign: 'center',
+    color: 'black',
+    fontSize: 18,
+    backgroundColor: "grey",
+    width: "100%",
+  },
+});
