18d9b3dd09917fd2e2bc4a3c5d9720e732954b61
Alejandro Chang <alejandrochang10@gmail.com>
2019-02-25 08:51:51 -0800

    added icon files for clone

---
+++ Philz Coffee Clone/Philz Coffee Clone/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  Philz Coffee Clone
+//
+//  Created by Alejandro Chang on 2/24/19.
+//  Copyright © 2019 Alejandro Chang. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
